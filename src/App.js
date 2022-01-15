6792dca6c241173a8c42d9d29e290d53fcf1fe08
geerma <geerm1997@gmail.com>
2022-01-14 20:57:11 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import {Routes} from "./Routes.jsx";
+
+/**
+ * The App.js is a file for the App component. It is the main component in React which acts as a container for all othercomponents.
+ * App.css is the corresponding css file associated with the App component.
+ * Typically, if you did a create-react-app command and make a brand new "Hello World" app, this page will be filled with the templated code.
+ *
+ * In this workshop, we decided to render the routes first with React Router.
+ * As you can see the, there is a <Routes/>
+ */
+
+function App() {
+  return (
+    <div className="App">
+      {/* TODO #1: Comment out the hello world h1 tag, import the Routes component and add to this div! */}
+      {/* <h1>Hello World!</h1> */}
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Routes.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { ResultsPage } from "./pages/ResultsPage";
+
+/**
+ * We use the react-router-dom library to simplify routing in our application.
+ * We use BrowserRouter, Switch, and Route.
+ *
+ * BrowserRouter is the router implementation for HTML5 browsers (https://v5.reactrouter.com/web/api/BrowserRouter)
+ * The Switch returns only the first machin route rather than all matching routes.
+ * The Link is the replacement for anchor tags.
+ *
+ * We give reach route a target component.
+ * So, in our example "/" will route to the "Home" component
+ */
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+
+        {/* TODO #2: Import the Results Page, look at the Home import statement on line 4 for reference!
+                     Add two routes following the format above!
+
+        - /home should route to the Home component
+        - /results should route to the ResultsPage component */}
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/results" component={ResultsPage} />
+
+      </Switch>
+    </BrowserRouter>
+  );
+};
